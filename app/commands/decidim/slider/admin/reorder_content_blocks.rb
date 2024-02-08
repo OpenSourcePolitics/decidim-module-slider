80500bd212d859579b2bff90f63178c3d1265f5f
AyakorK <guillaume.moret@yahoo.com>
2024-02-08 17:12:50 +0100

    feat: Add automatic refresh when an error occurs

--- app/commands/decidim/slider/admin/reorder_content_blocks.rb
+++ app/commands/decidim/slider/admin/reorder_content_blocks.rb
@@ -5,6 +5,15 @@ module Decidim
     module Admin
       # TODO: Remove when upgrading to Decidim 0.28
       class ReorderContentBlocks < Decidim::Admin::ReorderContentBlocks
+        def call
+          return broadcast(:invalid) if order.blank?
+
+          reorder_steps
+          broadcast(:ok)
+        rescue StandardError
+          broadcast(:invalid, "Error while reordering content blocks")
+        end
+
         private
 
         def set_new_weights
@@ -21,11 +30,21 @@ module Decidim
             next unless content_block.errors.any?
 
             error = content_block.errors.full_messages.join(", ")
+            title = translated_attribute(content_block.settings.title).presence || content_block.manifest_name
+            error = t("decidim.content_blocks.update.error", title: title, error: error)
+
           end
 
           if error.present?
             flash[:error] = error
+            raise StandardError, error
           end
+        end
+
+        def translated_attribute(attribute)
+          return unless attribute.is_a?(Hash)
+
+          attribute[I18n.locale] || attribute.values.first
         end
       end
     end
